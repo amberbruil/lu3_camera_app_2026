a4bd144260504dddb14d39750ca35adda4764ab9
amberbruil <amber.bruil@gmail.com>
2026-09-21 20:33:26 +0200

    unit test

--- app/src/test/java/com/example/lu3_camera_app/ExampleUnitTest.kt
+++ app/src/test/java/com/example/lu3_camera_app/ExampleUnitTest.kt
@@ -1,7 +1,7 @@
 package com.example.lu3_camera_app
 
 import org.junit.Test
-
+import com.example.lu3_camera_app.Model.ImageModel
 import org.junit.Assert.*
 
 /**
@@ -13,5 +13,21 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    /*
+        Testing the ImageModel class to ensure it's initialised correctly
+     */
+    @Test
+    fun imageModel_initialization_isCorrect() {
+        val name = "TestImage"
+        val uri = "content://media/external/images/media/1"
+        
+        // Creating an instance of ImageModel (Bitmap is null for local unit test)
+        val imageModel = ImageModel(name, null, uri)
+        
+        assertEquals(name, imageModel.imageName)
+        assertNull(imageModel.imageBitmap)
+        assertEquals(uri, imageModel.imageUri)
     }
 }
